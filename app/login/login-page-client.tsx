5584b0b7fd9127fb0bb139eaa664bb060d8e19fe
indeedOluwaloseyi <richard@qurulabs.com>
2026-03-20 06:58:22 +0100

    Added few changes

--- app/login/login-page-client.tsx
+++ app/login/login-page-client.tsx
@@ -45,7 +45,7 @@ export default function LoginPageClient() {
   return (
     <div className="min-h-screen flex">
       <div className="hidden lg:flex lg:w-1/2 relative">
-        <div className="absolute inset-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/home.jpeg')" }} />
+        <div className="absolute inset-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('https://image2url.com/r2/default/images/1773914396351-4ebaafbd-d8ad-4006-aa20-3fbeb0d83d97.jpg')" }} />
         <div className="absolute inset-0 bg-black/50" />
         <div className="relative z-10 flex flex-col items-center justify-center w-full h-full px-12 text-center text-white">
           <div className="flex items-center gap-3 mb-6">
@@ -105,7 +105,7 @@ export default function LoginPageClient() {
               {isLoading ? 'Signing in...' : 'Sign In'}
             </Button>
           </form>
-
+{/* 
           <div className="relative my-8">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-200" />
@@ -123,7 +123,7 @@ export default function LoginPageClient() {
               <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
             </svg>
             Continue with Google
-          </Button>
+          </Button> */}
 
           <p className="text-center text-gray-600 mt-8">
             Don&apos;t have an account?{' '}
